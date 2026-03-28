1a2d5c62bec2013f0f9db639ac978fe771c0f40a
mzg_al <muzgob@gmail.com>
2026-03-28 15:12:09 +0300

    small improvments

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,4 +1,6 @@
 import { defineConfig, devices } from '@playwright/test';
+import dotenv from 'dotenv';
+import path from 'path';
 
 /**
  * Read environment variables from file.
@@ -6,7 +8,7 @@ import { defineConfig, devices } from '@playwright/test';
  */
 // import dotenv from 'dotenv';
 // import path from 'path';
-// dotenv.config({ path: path.resolve(__dirname, '.env') });
+dotenv.config({ path: path.resolve(__dirname, '.env') });
 
 /**
  * See https://playwright.dev/docs/test-configuration.
@@ -20,14 +22,14 @@ export default defineConfig({
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? 1 : 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     ignoreHTTPSErrors: true,
     /* Base URL to use in actions like `await page.goto('')`. */
-    baseURL: 'https://litecart.stqa.ru/',
+    baseURL: process.env.BASE_URL,
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
@@ -48,11 +50,13 @@ export default defineConfig({
 
     {
       name: 'firefox',
+      dependencies: ["setupAuth"],
       use: { ...devices['Desktop Firefox'] },
     },
 
     {
       name: 'webkit',
+      dependencies: ["setupAuth"],
       use: { ...devices['Desktop Safari'] },
     },
 
--- setupAuth.ts
+++ setupAuth.ts
@@ -1,7 +1,12 @@
 import { test, expect } from './fixtures/fixtures.ts';
 
 test("User login", { tag: '@login' }, async ({ loginPage, page }) => {
-    await loginPage.loginAsUser('test@test.com', '1234567890');
+    const username = process.env.USER_EMAIL;
+    const password = process.env.USER_PASSWORD;
+    if (!username || !password) {
+        throw new Error('USER_EMAIL and USER_PASSWORD must be set in environment variables');
+    }
+    await loginPage.loginAsUser(username, password);
     await expect(page.locator('div.notice.success')).toContainText(/logged in as test test/i);
     await page.context().storageState({ path: './data/UserAuth.json' });
 });
